d3c7bf2c2d43b95e2f6b9e03dd2363772e6541d0
farkasadrianflybike <farkasadrian.flybike@hotmail.com>
2020-03-24 23:42:40 +0100

    added converter for four digits

--- js-numerals/src/utils/converter.js
+++ js-numerals/src/utils/converter.js
@@ -3,6 +3,7 @@ import { FROM_0_TO_19, TENS, SCALE } from "./BasicNums";
 export const numToWord = number => {
   const num = Math.abs(number);
   if (num === 0) return FROM_0_TO_19[0];
+  if (num.toString().length === 4) return convertFourDigits(num);
   const word = convertNumber(num, 0);
   return number < 0 ? `minus ${word}` : word;
 };
@@ -19,6 +20,7 @@ const convertNumber = (num, index) => {
 };
 
 const convertToTwoDigits = num => {
+  if (num === 0) return "";
   if (num < 20) return FROM_0_TO_19[num];
 
   const tens_index = getRoundedDistributedNum(num, 10) - 1;
@@ -28,14 +30,23 @@ const convertToTwoDigits = num => {
 };
 
 const convertToThreeDigits = num => {
-  if (num === 0) return "";
-
-  if (num.toString().length === 1) return FROM_0_TO_19[num];
-  if (num.toString().length === 2) return convertToTwoDigits(num);
+  if (num.toString().length <= 2) return convertToTwoDigits(num);
   if (num % 100 === 0) return `${FROM_0_TO_19[num / 100]} hundred`;
   return `${
     FROM_0_TO_19[getRoundedDistributedNum(num, 100)]
   } hundred and ${convertToTwoDigits(num % 100)}`;
+};
+
+const convertFourDigits = num => {
+  const firstHalf = getRoundedDistributedNum(num, 100);
+  const secondHalf = num % 100;
+  if (firstHalf % 10 !== 0) {
+    return `${convertToTwoDigits(firstHalf)}
+     hundred and ${convertToTwoDigits(secondHalf)}`;
+  }
+  let word = convertNumber(num, 0);
+  if (secondHalf) word = word.replace(SCALE[0], `${SCALE[0]} and`);
+  return word;
 };
 
 const getRoundedDistributedNum = (toDivide, divideWith) => {
