27907874101c29fecac809c940a3780405903c0e
farkasadrianflybike <farkasadrian.flybike@hotmail.com>
2020-03-24 23:44:33 +0100

    readability

--- js-numerals/src/utils/converter.js
+++ js-numerals/src/utils/converter.js
@@ -2,8 +2,10 @@ import { FROM_0_TO_19, TENS, SCALE } from "./BasicNums";
 
 export const numToWord = number => {
   const num = Math.abs(number);
+
   if (num === 0) return FROM_0_TO_19[0];
   if (num.toString().length === 4) return convertFourDigits(num);
+
   const word = convertNumber(num, 0);
   return number < 0 ? `minus ${word}` : word;
 };
@@ -32,6 +34,7 @@ const convertToTwoDigits = num => {
 const convertToThreeDigits = num => {
   if (num.toString().length <= 2) return convertToTwoDigits(num);
   if (num % 100 === 0) return `${FROM_0_TO_19[num / 100]} hundred`;
+
   return `${
     FROM_0_TO_19[getRoundedDistributedNum(num, 100)]
   } hundred and ${convertToTwoDigits(num % 100)}`;
@@ -40,11 +43,14 @@ const convertToThreeDigits = num => {
 const convertFourDigits = num => {
   const firstHalf = getRoundedDistributedNum(num, 100);
   const secondHalf = num % 100;
+
   if (firstHalf % 10 !== 0) {
     return `${convertToTwoDigits(firstHalf)}
      hundred and ${convertToTwoDigits(secondHalf)}`;
   }
+
   let word = convertNumber(num, 0);
+
   if (secondHalf) word = word.replace(SCALE[0], `${SCALE[0]} and`);
   return word;
 };
