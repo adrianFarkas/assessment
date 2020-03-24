b1c2f3be275d52964b824b5d85fbb1f777f9efee
farkasadrianflybike <farkasadrian.flybike@hotmail.com>
2020-03-24 16:34:46 +0100

    converts three digits

--- js-numerals/src/utils/converter.js
+++ js-numerals/src/utils/converter.js
@@ -3,6 +3,7 @@ import { FROM_0_TO_19, TENS } from "./BasicNums";
 export const numToWord = num => {
   let word = "";
   if (num < 100 && num > -100) word = convertToTwoDigits(num);
+  else word = convertToThreeDigits(num);
   if (num < 0) word = `minus ${word}`;
   return word;
 };
@@ -11,8 +12,20 @@ const convertToTwoDigits = num => {
   num = Math.abs(num);
   if (num < 20) return FROM_0_TO_19[num];
 
-  const tens_index = Math.floor(num / 10 - 1);
+  const tens_index = getRoundedDistributedNum(num, 10) - 1;
 
   if (num % 10 === 0) return TENS[tens_index];
   return `${TENS[tens_index]}-${FROM_0_TO_19[num % 10]}`;
+};
+
+const convertToThreeDigits = num => {
+  num = Math.abs(num);
+  if (num % 100 === 0) return `${FROM_0_TO_19[num / 100]} hundred`;
+  return `${
+    FROM_0_TO_19[getRoundedDistributedNum(num, 100)]
+  } hundred and ${convertToTwoDigits(num % 100)}`;
+};
+
+const getRoundedDistributedNum = (toDivide, divideWith) => {
+  return Math.floor(toDivide / divideWith);
 };
--- js-numerals/src/utils/converter.test.js
+++ js-numerals/src/utils/converter.test.js
@@ -24,8 +24,21 @@ describe("Number to word", () => {
     expect(numToWord(60)).toEqual("sixty");
     expect(numToWord(90)).toEqual("ninety");
   });
+  test("Three digits", () => {
+    expect(numToWord(153)).toEqual("one hundred and fifty-three");
+    expect(numToWord(440)).toEqual("four hundred and forty");
+    expect(numToWord(627)).toEqual("six hundred and twenty-seven");
+    expect(numToWord(710)).toEqual("seven hundred and ten");
+  });
+  test("Three digits divisible by 10", () => {
+    expect(numToWord(100)).toEqual("one hundred");
+    expect(numToWord(500)).toEqual("five hundred");
+    expect(numToWord(700)).toEqual("seven hundred");
+    expect(numToWord(900)).toEqual("nine hundred");
+  });
   test("Negatives", () => {
     expect(numToWord(-20)).toEqual("minus twenty");
     expect(numToWord(-53)).toEqual("minus fifty-three");
+    expect(numToWord(-433)).toEqual("minus four hundred and thirty-three");
   });
 });
