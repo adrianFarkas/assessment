58506eb67332bf4d4e0e9ae7d6c4c771e2564878
adrianFarkas <farkasadrian1996@gmail.com>
2020-04-02 22:47:36 +0200

    modified to not overwrite existing query strings

--- js-users/src/components/utils/page-selector/PageSelector.js
+++ js-users/src/components/utils/page-selector/PageSelector.js
@@ -1,17 +1,18 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import useQueryParams from "hooks/useQueryParams";
-import { Redirect, Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 
 const Selector = styled.div`
   display: flex;
 `;
 
-const PageNum = styled(Link)`
+const PageNum = styled.div`
   width: 30px;
   height: 30px;
   margin: 2px;
   display: flex;
+  cursor: pointer;
   align-items: center;
   justify-content: center;
   border-radius: 6px;
@@ -34,7 +35,8 @@ const PageNum = styled(Link)`
 
 const PageSelector = ({ path = "/", itemPerPage, totalItems }) => {
   const totalPages = Math.ceil(totalItems / itemPerPage);
-  const { page } = useQueryParams();
+  const queryParams = useQueryParams();
+  const { page } = queryParams;
   const actPage = page ? parseInt(page) : 1;
 
   const pages = [];
@@ -45,6 +47,11 @@ const PageSelector = ({ path = "/", itemPerPage, totalItems }) => {
     pages.push(i);
   }
 
+  const changePage = page => {
+    if (page === 1) queryParams.remove("page");
+    else queryParams.set("page", page);
+  };
+
   if (actPage <= 0 || actPage > totalPages) {
     return <Redirect to={path} />;
   }
@@ -53,9 +60,9 @@ const PageSelector = ({ path = "/", itemPerPage, totalItems }) => {
     <Selector>
       {pages.map(num => (
         <PageNum
-          to={num > 1 ? `${path}?page=${num}` : path}
           key={num}
           selected={actPage === num}
+          onClick={() => changePage(num)}
         >
           {num}
         </PageNum>
