157d58cd39ea05c743e1cb3ab425cc3e460d6f0e
adrianFarkas <farkasadrian1996@gmail.com>
2020-04-02 12:31:36 +0200

    added colors

--- js-users/src/index.js
+++ js-users/src/index.js
@@ -3,20 +3,23 @@ import ReactDOM from "react-dom";
 import Users from "pages/Users";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import RootContextProvider from "contexts/RootContext";
-import GlobalStyle from "style/globalStyle";
+import GlobalStyle, { colors } from "style/globalStyle";
 import Edit from "pages/Edit";
 import New from "pages/New";
+import { ThemeProvider } from "styled-components";
 
 const routes = (
   <RootContextProvider>
-    <GlobalStyle />
-    <Router>
-      <Switch>
-        <Route path="/new" component={New} />
-        <Route path="/edit/:userId" component={Edit} />
-        <Route exact path="/" component={Users} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={{ colors }}>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route path="/new" component={New} />
+          <Route path="/edit/:userId" component={Edit} />
+          <Route exact path="/" component={Users} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   </RootContextProvider>
 );
 
--- js-users/src/style/globalStyle.js
+++ js-users/src/style/globalStyle.js
@@ -3,6 +3,14 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
     html, body {
         margin: 0;
+        font-family: sans-serif;
+        background-attachment: fixed;
+        background: rgb(67, 208, 226);
+        background: linear-gradient(
+            133deg,
+            rgba(67, 208, 226, 1) 0%,
+            rgba(72, 146, 226, 1) 100%
+        );
     }
     a {
         text-decoration: none;
@@ -16,5 +24,12 @@ const GlobalStyle = createGlobalStyle`
         outline: none;
     }
 `;
+
+export const colors = {
+  light: "#ffffff",
+  shadow: "#00000030",
+  blue: "#2a6b89",
+  transparentBlue: "#398fb790"
+};
 
 export default GlobalStyle;
